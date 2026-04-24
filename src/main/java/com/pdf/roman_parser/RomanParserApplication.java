8b22a0e9fa97ac961288b05036566344dac1d82a
RomanMelnikov77 <79645773155@yandex.ru>
2026-04-24 19:37:42 +0300

    init commit

---
+++ src/main/java/com/pdf/roman_parser/RomanParserApplication.java
@@ -0,0 +1,13 @@
+package com.pdf.roman_parser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RomanParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RomanParserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdf/roman_parser/RomanParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdf.roman_parser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RomanParserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
